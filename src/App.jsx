1d9447dbdfd9c291dd17fead6c28bb48883b6d85
Catarina Guimaraes <catarina.guimaraes15@gmail.com>
2024-09-14 13:05:02 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return (
+      <header>
+        <nav id="navbar">
+          <div className="nav-brand">
+            <img src="" alt="" />
+            <h1>Space Flight News</h1>
+          </div>
+          <ul className="nav-list">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+
+        </nav>
+      </header>
+    );
+  }
+}
+
+export default App;
